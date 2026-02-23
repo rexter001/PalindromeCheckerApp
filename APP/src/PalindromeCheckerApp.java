87176da1031368b0a5dd09ace62de7961df38e53
rexter001 <khajamastanshaik001>
2026-02-23 11:28:36 +0530

    rexter001 Added UC10-CaseAndSpce

--- APP/src/PalindromeCheckerApp.java
+++ APP/src/PalindromeCheckerApp.java
@@ -2,23 +2,6 @@ import java.util.Scanner;
 
 public class PalindromeCheckerApp {
 
-    // Recursive method
-    public static boolean isPalindrome(String str, int start, int end) {
-
-        // Base condition
-        if (start >= end) {
-            return true;
-        }
-
-        // Compare start and end characters
-        if (str.charAt(start) != str.charAt(end)) {
-            return false;
-        }
-
-        // Recursive call
-        return isPalindrome(str, start + 1, end - 1);
-    }
-
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
@@ -27,14 +10,29 @@ public class PalindromeCheckerApp {
         System.out.print("Enter a string: ");
         String str = sc.nextLine();
 
-        // Check palindrome using recursion
-        boolean result = isPalindrome(str, 0, str.length() - 1);
+        // Normalize string
+        // remove spaces and convert to lowercase
+        String normalized = str.replaceAll("\\s+", "").toLowerCase();
+
+        // Palindrome check
+        int start = 0;
+        int end = normalized.length() - 1;
+        boolean isPalindrome = true;
+
+        while (start < end) {
+            if (normalized.charAt(start) != normalized.charAt(end)) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
 
         // Print result
-        if (result) {
-            System.out.println(str + " is a Palindrome");
+        if (isPalindrome) {
+            System.out.println("\"" + str + "\" is a Palindrome");
         } else {
-            System.out.println(str + " is NOT a Palindrome");
+            System.out.println("\"" + str + "\" is NOT a Palindrome");
         }
 
         sc.close();
