7f40eed54a701a552c18a12a20f5c3f51a1da481
rexter001 <khajamastanshaik001>
2026-02-23 10:44:45 +0530

    rexter001 Added UC5-Stack

--- APP/src/PalindromeCheckerApp.java
+++ APP/src/PalindromeCheckerApp.java
@@ -1,4 +1,38 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
+import java.util.Stack;
 
 public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        // Scanner for user input
+        Scanner sc = new Scanner(System.in);
+
+        // Take input
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        // Create Stack
+        Stack<Character> stack = new Stack<>();
+
+        // Push characters into stack
+        for (int i = 0; i < str.length(); i++) {
+            stack.push(str.charAt(i));
+        }
+
+        // Pop characters and build reversed string
+        String rev = "";
+        while (!stack.isEmpty()) {
+            rev = rev + stack.pop();
+        }
+
+        // Compare original and reversed
+        if (str.equals(rev)) {
+            System.out.println(str + " is a Palindrome");
+        } else {
+            System.out.println(str + " is NOT a Palindrome");
+        }
+
+        sc.close();
+    }
 }
